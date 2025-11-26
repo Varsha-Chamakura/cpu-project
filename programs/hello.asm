1266c07f30588ac4199fdbc3ba377de993853d6b
Varsha Chamakura <varshachamakura4@gmail.com>
2025-11-25 21:15:55 -0800

    Update hello.asm

--- programs/hello.asm
+++ programs/hello.asm
@@ -1,1 +1,48 @@
+; hello.asm - prints "Hello, World!"
+; Program origin assumed 0x0100
+
+START:
+    LDAI #'H'
+    STA 0x00FF
+
+    LDAI #'e'
+    STA 0x00FF
+
+    LDAI #'l'
+    STA 0x00FF
+
+    LDAI #'l'
+    STA 0x00FF
+
+    LDAI #'o'
+    STA 0x00FF
+
+    LDAI #','
+    STA 0x00FF
+
+    LDAI #' '
+    STA 0x00FF
+
+    LDAI #'W'
+    STA 0x00FF
+
+    LDAI #'o'
+    STA 0x00FF
+
+    LDAI #'r'
+    STA 0x00FF
+
+    LDAI #'l'
+    STA 0x00FF
+
+    LDAI #'d'
+    STA 0x00FF
+
+    LDAI #'!'
+    STA 0x00FF
+
+    LDAI #'\n'
+    STA 0x00FF
+
+    HLT
 
